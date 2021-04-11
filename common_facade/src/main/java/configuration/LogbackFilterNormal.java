29066eecb98d17806eb07097ad635d41f9bdbe0b
slowice <448220654@qq.com>
2021-04-11 20:07:21 +0800

    简单整理项目

---
+++ common_facade/src/main/java/configuration/LogbackFilterNormal.java
@@ -0,0 +1,16 @@
+package configuration;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+
+public class LogbackFilterNormal extends Filter<ILoggingEvent> {
+    @Override
+    public FilterReply decide(ILoggingEvent iLoggingEvent) {
+        boolean fromXB = iLoggingEvent.getLoggerName().startsWith("xb");
+        if(fromXB){
+            return FilterReply.DENY;
+        }
+        return FilterReply.ACCEPT;
+    }
+}
